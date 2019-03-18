0417ece473a1c3b1c770c8666d6e44549845911e
Tim Süberkrüb <dev@timsueberkrueb.io>
2019-03-18 18:10:51 +0100

    Add documentation about enter and leave

--- visit/src/lib.rs
+++ visit/src/lib.rs
@@ -104,6 +104,39 @@ use syn::visit::Visit;
 /// visit also generates some default implementations for common collections and `Option<T>`. Primitive types are
 /// ignored (visit generates an empty accept trait implementation for them).
 ///
+/// # Customizing `#![visitor]`
+///
+/// The `visitor` attribute supports the following parameters:
+///
+/// * `name`: identifier for generated visitor trait, e.g. `Visitor`
+/// * `public`: whether the generated visitor and accept visitor traits should be `pub`
+/// * `leave`: prefix (valid Rust identifier)
+/// * `enter`: prefix (valid Rust identifier)
+///
+/// Setting `leave` to an identifier (e.g. `visit`) generates visit functions prefixed with the given identifier
+/// (e.g. `visit_foo`, `visit_bar`). `leave` functions get called when the visitor *leaves* a given node,
+/// that is, when all its children have been visited. This is the default behavior of the standard visitor pattern.
+///
+/// When both `leave` and `enter` are omitted, `leave` defaults to `visit`.
+///
+/// `enter` functions on the contrary get called, when the visitor *enters* a given node, before any of its children get
+/// visited.
+///
+/// The following code sample illustrates this behaviour:
+///
+/// ```ignore
+/// impl AcceptVisitor for Bar {
+///     fn accept<V: Visitor>(&self, visitor: &mut V) {
+///         visitor.enter_bar(self);
+///         self.a.accept(visitor);
+///         self.b.accept(visitor);
+///         visitor.leave_bar(self);
+///     }
+/// }
+/// ```
+///
+/// The concept of `enter` functions is part of the [Hierarchical Visitor Pattern](http://wiki.c2.com/?HierarchicalVisitorPattern).
+///
 #[proc_macro]
 pub fn visit(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let mut file: syn::File = syn::parse2(input.into()).unwrap();
